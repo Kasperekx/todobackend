1f906e9cd336113dde6b1445d7eb9e8955c74070
Kasperekx <piotr.kasperek92@gmail.com>
2022-07-08 11:08:57 +0200

    swagger update

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const Task = require("../models/TaskModel");
-const { requireAuth } = require("../middlewares/authMiddlewares")
+const { requireAuth } = require("../middlewares/authMiddlewares");
 const {
   getUniqueTaskController,
   getAllTasksController,
@@ -12,12 +12,12 @@ const router = express.Router();
 
 router.get("/", requireAuth, getAllTasksController);
 
-router.get("/:id", requireAuth,getUniqueTaskController);
+router.get("/:id", requireAuth, getUniqueTaskController);
 
-router.post("/", requireAuth,createTaskController);
+router.post("/create", requireAuth, createTaskController);
 
-router.delete("/:id", requireAuth,deleteTaskController);
+router.delete("/delete/:id", requireAuth, deleteTaskController);
 
-router.patch("/:id", requireAuth,updateTaskController);
+router.patch("/update/:id", requireAuth, updateTaskController);
 
 module.exports = router;
